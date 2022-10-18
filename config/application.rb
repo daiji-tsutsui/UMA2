2b41f10a1f5690188a21c9896f00049e109f03bb
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2022-10-19 01:46:58 +0900

    Add database setting and got first access

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module UMA2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.colorize_logging = false
   end
 end
