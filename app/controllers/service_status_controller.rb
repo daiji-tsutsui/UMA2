05130252c4185b7344a816fcfd8602fd181ff62a
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2022-10-19 02:23:48 +0900

    Add service status

---
+++ app/controllers/service_status_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Service Status API Controller
+class ServiceStatusController < ApplicationController
+  def status
+    @details = {}
+    @errors = []
+
+    check_status('db') { check_db }
+
+    res = if @errors.empty?
+            { app_name: 'UMA2', status: 'OK', details: @details }
+          else
+            { app_name: 'UMA2', status: 'NG', details: @details, errors: @errors }
+          end
+    Rails.logger.info "service status: #{res}"
+    render json: res
+  end
+
+  private
+
+  CHECK_TIMEOUT = 20.seconds
+
+  def check_status(target, &)
+    @details[target] ||= {}
+    t = Time.current
+    Timeout.timeout(CHECK_TIMEOUT, &)
+    @details[target][:time] = Time.current - t
+  rescue StandardError => e
+    @errors.push "#{target}: #{e.message}"
+  end
+
+  def check_db
+    # Do nothing now
+  end
+end
