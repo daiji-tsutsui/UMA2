2f23cef907de1159d9c3a0d6b3cdf49fd9b3e530
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2022-10-22 00:56:28 +0900

    Modify processing of race record

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -2,19 +2,46 @@
 
 # Race information Controller
 class RaceController < ApplicationController
+  before_action :make_caches, only: %i[index show]
+
   def index
-    @races = Race.all
+    @races = Race.all.map { |race| format(race) }
   end
 
   def show
-    base_data = Race.find(params[:id])
-    @race = {
-      name:    base_data.name,
-      date:    RaceDate.find(base_data.race_date_id).value,
-      course:  Course.find(base_data.course_id).name,
-      number:  base_data.number,
-      class:   RaceClass.find(base_data.race_class_id).name,
-      weather: base_data.weather,
+    @race = format(Race.find_by(id: params[:id]))
+  end
+
+  private
+
+  def format(record)
+    return format_for_nil if record.nil?
+
+    {
+      name:    record.name,
+      date:    get_date_or_failover(record.race_date_id),
+      course:  @courses[record.course_id],
+      number:  record.number,
+      class:   @classes[record.race_class_id],
+      weather: record.weather,
     }
+  end
+
+  def format_for_nil
+    %i[name date course number class weather].to_h { |col| [col, 'No Data'] }
+  end
+
+  def make_caches
+    return unless @courses.nil? || @classes.nil?
+
+    @courses = Course.all.to_h { |course| [course.id, course.name] }
+    @classes = RaceClass.all.to_h { |elem| [elem.id, elem.name] }
+  end
+
+  def get_date_or_failover(date_id)
+    @dates = {} if @dates.nil?
+    return @dates[date_id] if @dates.key?(date_id)
+
+    @dates[date_id] = RaceDate.find(date_id).value
   end
 end
