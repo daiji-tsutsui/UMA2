db5b55eb305d48adf401050fb3b8aa20bed5bf73
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2022-10-24 23:25:44 +0900

    Add test for RacesController

--- spec/requests/races_spec.rb
+++ spec/requests/races_spec.rb
@@ -4,16 +4,37 @@ require 'rails_helper'
 
 RSpec.describe 'Races', type: :request do
   describe 'GET /races' do
+    before { get races_path }
+
     it 'should response success' do
-      get races_path
-      assert_response :success
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders table of races' do
+      expect(response.body).to include('レース名')
+      expect(response.body).to include('出走日')
+      expect(response.body).to include('2022-10-22')
+      expect(response.body).to include('札幌')
+      expect(response.body).to include('Test2')
+      expect(response.body).to include('G2')
     end
   end
 
   describe 'GET /races/:id' do
+    before { get race_path(1) }
+
     it 'should response success' do
-      get race_path(1)
-      assert_response :success
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders table of a race' do
+      expect(response.body).to include('格付')
+      expect(response.body).to include('天気')
+      expect(response.body).to include('Test1')
+      expect(response.body).to include('2022-10-22')
+      expect(response.body).to include('札幌')
+      expect(response.body).to include('11R')
+      expect(response.body).to include('G1')
     end
   end
 end
