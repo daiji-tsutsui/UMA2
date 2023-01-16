2dc4d3a7b5af510b058a580046a47e529fd36663
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2023-01-17 01:32:38 +0900

    Add horses table

---
+++ db/migrate/20230116162918_create_horses.rb
@@ -0,0 +1,9 @@
+class CreateHorses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :horses do |t|
+      t.text :name, null: false
+      t.timestamps
+    end
+    add_index :horses, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_094114) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_162918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_094114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_courses_on_name", unique: true
+  end
+
+  create_table "horses", force: :cascade do |t|
+    t.text "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_horses_on_name", unique: true
   end
 
   create_table "race_classes", force: :cascade do |t|
