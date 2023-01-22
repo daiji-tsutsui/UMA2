3e45a40638855e37a3852a346925bd3e8edf16b1
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2023-01-23 00:35:21 +0900

    Add header and footer templates

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -16,17 +16,15 @@ class RaceController < ApplicationController
     @race = Race.where.associated(:race_date)
                 .select('races.*, race_dates.value AS date')
                 .find_by(id: params[:id])
-    if @race.nil?
-      flash[:danger] = ERROR_MESSAGE_NO_SUCH_RACE
-      redirect_to races_path and return
-    end
+    return unless @race.nil?
+
+    flash[:danger] = ERROR_MESSAGE_NO_SUCH_RACE
+    redirect_to races_path and return
   end
 
   private
 
   def load_static_record
-    return unless @courses.nil? || @classes.nil?
-
     @courses = Course.all.to_h { |course| [course.id, course.name] }
     @classes = RaceClass.all.to_h { |elem| [elem.id, elem.name] }
   end
