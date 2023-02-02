a3515f42b2cb41acd217bf708d56564d28661f51
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2023-02-03 01:05:03 +0900

    Add unittest for Horse View

--- spec/requests/races_spec.rb
+++ spec/requests/races_spec.rb
@@ -34,14 +34,22 @@ RSpec.describe 'Races', type: :request do
     it 'renders table of a race' do
       # h1 tag
       expect(response.body).to include('Test1')
-      # table headers
+      # race table headers
       expect(response.body).to include('出走時刻')
       expect(response.body).to include('レース番号')
       expect(response.body).to include('距離')
-      # table rows
+      # race table rows
       expect(response.body).to include('2022-10-22')
       expect(response.body).to include('札幌')
       expect(response.body).to include('11R')
+      # horse table headers
+      expect(response.body).to include('馬番')
+      expect(response.body).to include('性齢')
+      expect(response.body).to include('騎手')
+      # horse table rows
+      expect(response.body).to include('タニノギムレット')
+      expect(response.body).to include('牡4')
+      expect(response.body).to include('横山武')
       # class identifier
       expect(response.body).to include('raceclass-G1')
     end
