4fbb8986f22707c29c8de62978754bd00e90cf2c
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2023-02-05 01:28:57 +0900

    Add search form for Race View

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -5,10 +5,9 @@ class RaceController < ApplicationController
   ERROR_MESSAGE_NO_SUCH_RACE = 'No such a race...'
 
   def index
-    @races = Race.all.preload(:race_date, :race_class, :course)
-                 .order(race_date_id: :DESC)
-                 .order(course_id: :ASC)
-                 .order(number: :ASC)
+    @races = Race.search(search_params)
+                 .sort_by_date
+                 .preload(:race_date, :race_class, :course)
                  .paginate(page: params[:page])
   end
 
@@ -17,5 +16,11 @@ class RaceController < ApplicationController
 
     flash[:danger] = ERROR_MESSAGE_NO_SUCH_RACE
     redirect_to races_path and return
+  end
+
+  private
+
+  def search_params
+    params.permit(:name, :date, :course, :number, race_class: [])
   end
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
+# Model class for 'races' table
 class Race < ApplicationRecord
   belongs_to :race_date
   belongs_to :course
   belongs_to :race_class
   has_many   :race_horses
+
+  scope :sort_by_date, -> { order(race_date_id: :DESC, course_id: :ASC, number: :ASC) }
+
+  def self.search(params)
+    # TODO: dateに対応したい．2023-01とかで検索できるように
+    result = Race.all
+    result = result.where('name LIKE ?', "%#{params[:name]}%") unless params[:name].blank?
+    result = result.where(course_id: params[:course])          unless params[:course].blank?
+    result = result.where(number: params[:number])             unless params[:number].blank?
+    result = result.where(race_class_id: params[:race_class])  unless params[:race_class].blank?
+    result
+  end
 end
