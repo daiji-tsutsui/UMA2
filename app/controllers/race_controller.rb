5a76668acabb16414439174559031a79a3343770
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2023-02-12 03:50:44 +0900

    Add odds history table to Race View

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -12,10 +12,12 @@ class RaceController < ApplicationController
   end
 
   def show
-    @race = Race.find_by(id: params[:id]) and return
+    @race = Race.includes(race_horses: :horse)
+                .find(params[:id])
+    return unless @race.nil?
 
     flash[:danger] = ERROR_MESSAGE_NO_SUCH_RACE
-    redirect_to races_path and return
+    redirect_to races_path
   end
 
   private
