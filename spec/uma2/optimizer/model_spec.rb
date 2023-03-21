4445484a0b3be55a8c9ce0749f1bea302aa94acd
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2023-03-22 01:22:45 +0900

    Fix unittest of Uma2::Optimizer::Model

--- spec/uma2/optimizer/model_spec.rb
+++ spec/uma2/optimizer/model_spec.rb
@@ -4,49 +4,6 @@ require 'rails_helper'
 require 'uma2/optimizer/model'
 
 RSpec.describe 'Uma2::Optimizer::Model' do
-  xdescribe '#strategy' do
-    subject { Uma2::Optimizer::Model.strategy(odds, b, t) }
-
-    context 'with uniform odds and t' do
-      let(:odds) { [2.4, 2.4, 2.4] }
-      let(:b)    { 1.0 }
-      let(:t)    { Uma2::Probability.new([0.33, 0.33, 0.33]) }
-      it 'gives a probability distribution' do
-        is_expected.to be_a Uma2::Probability
-      end
-      it 'gives a uniform distribution' do
-        s = subject
-        expect(s[0]).to eq s[1]
-        expect(s[0]).to eq s[2]
-      end
-    end
-
-    context 'with nonuniform odds and t' do
-      let(:odds) { [3.2, 3.2, 1.6] }
-      let(:t)    { Uma2::Probability.new([0.4, 0.2, 0.4]) }
-
-      context 'if b is large' do
-        let(:b) { 10.0 }
-        it 'gives a sharp distribution' do
-          s = subject
-          expect(s[0]).to be > s[1]
-          expect(s[0]).to be > s[2]
-          expect(s[0]).to within(0.01).of(1.0)
-        end
-      end
-
-      context 'if b is small' do
-        let(:b) { 0.01 }
-        it 'gives a flat distribution' do
-          s = subject
-          expect(s[0]).to be > s[1]
-          expect(s[0]).to be > s[2]
-          expect(s[0]).to within(0.01).of(0.33)
-        end
-      end
-    end
-  end
-
   describe '#new' do
     subject { @model = Uma2::Optimizer::Model.new }
 
@@ -121,6 +78,50 @@ RSpec.describe 'Uma2::Optimizer::Model' do
       let(:odds_list) { odds_list_data }
       it 'outputs a positive value' do
         is_expected.to be > 0.1
+      end
+    end
+  end
+
+  describe '#strategy' do
+    before { @model = Uma2::Optimizer::Model.new }
+    subject { @model.strategy(odds, b, t) }
+
+    context 'with uniform odds and t' do
+      let(:odds) { [2.4, 2.4, 2.4] }
+      let(:b)    { 1.0 }
+      let(:t)    { Uma2::Probability.new([0.33, 0.33, 0.33]) }
+      it 'gives a probability distribution' do
+        is_expected.to be_a Uma2::Probability
+      end
+      it 'gives a uniform distribution' do
+        s = subject
+        expect(s[0]).to eq s[1]
+        expect(s[0]).to eq s[2]
+      end
+    end
+
+    context 'with nonuniform odds and t' do
+      let(:odds) { [3.2, 3.2, 1.6] }
+      let(:t)    { Uma2::Probability.new([0.4, 0.2, 0.4]) }
+
+      context 'if b is large' do
+        let(:b) { 10.0 }
+        it 'gives a sharp distribution' do
+          s = subject
+          expect(s[0]).to be > s[1]
+          expect(s[0]).to be > s[2]
+          expect(s[0]).to within(0.01).of(1.0)
+        end
+      end
+
+      context 'if b is small' do
+        let(:b) { 0.01 }
+        it 'gives a flat distribution' do
+          s = subject
+          expect(s[0]).to be > s[1]
+          expect(s[0]).to be > s[2]
+          expect(s[0]).to within(0.01).of(0.33)
+        end
       end
     end
   end
